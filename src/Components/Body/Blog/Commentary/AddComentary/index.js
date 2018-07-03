1e7a512f8d45d71d2b2720ed606cc52b0256c70d
George701 <chernikov.georgii@gmail.com>
2018-07-03 20:33:58 +0300

    Modified AddCommentary

--- src/Components/Body/Blog/Commentary/AddComentary/index.js
+++ src/Components/Body/Blog/Commentary/AddComentary/index.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './AddCommentary.css';
 
 class AddCommentary extends Component {
+    static propTypes = {
+
+    };
+
+    state = {
+        user: '',
+        text: ''
+    };
 
     render(){
         return(
@@ -11,16 +20,58 @@ class AddCommentary extends Component {
                 </p>
                 <form action="#" id="commentform">
                     <span>Your Name</span><br/>
-                    <input type="text" name="name"/>
+                    <input
+                        value={this.state.user}
+                        onChange={this.handleChange('user')}
+                        className={this.getClassName('user')}
+                    />
                     <br/>
                     <span>Your Message</span><br/>
-                    <textarea name="comment" form="commentform" cols="30" rows="4">Enter yor commentary here...</textarea>
+                    <textarea
+                        name="comment"
+                        form="commentform"
+                        cols="30"
+                        rows="4"
+                        value={this.state.text}
+                        onChange={this.handleChange('text')}
+                        className={this.getClassName('text')}
+                    >Enter yor commentary here...</textarea>
                     <br/>
-                    <input type="submit"/>
+                    <input type="submit" onClick={this.handleSubmit}/>
                 </form>
             </div>
         )
     }
+
+    handleSubmit = ev =>{
+        ev.preventDefault();
+        this.setState({
+            user: '',
+            text: ''
+        })
+    };
+
+    getClassName = type => this.state[type].length && this.state[type].length < limits[type].min ? 'form-input__error' : '';
+
+    handleChange = type => ev => {
+        const {value} = ev.target;
+        if (value.length > limits[type].max) return;
+        this.setState({
+            [type]: value
+        })
+    };
+
 }
+
+const limits = {
+  user: {
+      min: 5,
+      max: 15
+  },
+  text: {
+      min: 20,
+      max: 180
+  }
+};
 
 export default AddCommentary;
